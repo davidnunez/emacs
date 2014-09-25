94a721b62b859ed810e650fd152b25f4c484bfd6
David Nunez <david@davidnunez.com>
2014-09-25 12:00:52 -0400

    adding elpa and dayone

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,19 @@
 ;; install dependencies with el-get
 (require 'el-get-settings)
 
+(require 'package)
+(add-to-list 'package-archives
+  ;; The 't' means to append, so that MELPA comes after the more
+  ;; stable ELPA archive.
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; Add ELPA if necessary. Looking at the El-Get package.rcp recipe in
+;; ~/local/opt/el-get/recipes it seems this is probably unnecessary.
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
 ;---------------;
 ;;; Utilities ;;;
 ;---------------;
